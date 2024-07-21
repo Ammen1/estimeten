a58b925d72290069897a3a2f589497f638c4ed13
Tamirat <amenguda@gmail.com>
2024-07-21 14:30:42 +0300

    Update Dashboard Component for Diabetes Management Project

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashboardComp from "../components/DashboardComp";
+
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+      {/* posts... */}
+      {tab === "projects" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
+     
+    </div>
+  );
+}
