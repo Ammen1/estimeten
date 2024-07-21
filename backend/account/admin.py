f990041c12ef3bb182e538d4ad4fd396cf7f2410
Tamirat <amenguda@gmail.com>
2024-07-21 15:38:11 +0300

    create admin for user account

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.account.models import (
+    NewUser,
+    AdminUser,
+)
 
-# Register your models here.
+admin.site.register(NewUser)
+admin.site.register(AdminUser)
+
