84975d032fa4c9682c083fefd9fba2d53cf50122
Tamirat <amenguda@gmail.com>
2024-07-21 15:37:26 +0300

    create urls for user account

---
+++ backend/account/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from .views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+app_name = 'account'
+
+urlpatterns = [
+    path('create/', Register.as_view(), name="create_user"),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', GetUserView.as_view(), name='user'),
+    path('users/', Allusers.as_view(), name='users'),
+    path('deleteuser/<int:pk>/', DeteleUser.as_view(), name='deteleusers'),
+    path('edituser/<int:pk>/', EditUser.as_view(), name='edituser'),
+    path('admin/', GetAdmins.as_view(), name='admin'),
+    path('admin/create/', CreateAdmin.as_view(), name='create'),
+
+
+]
