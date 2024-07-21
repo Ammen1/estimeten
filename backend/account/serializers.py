a2bd1be2ed6e4a5df88240a6c3ea7b8746e02879
Tamirat <amenguda@gmail.com>
2024-07-21 15:40:34 +0300

    account folder refine

---
+++ backend/account/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from core.account.models import NewUser, AdminUser
+
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdminUser
+        fields = '__all__'
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = NewUser
+        fields = ('id', 'phone', 'email', 'password1', 'password2',
+                  'is_active', 'is_superuser',  'last_login', 'is_admin_user', 'is_driver', 'user_photo', 'address')
+        extra_kwargs = {
+            'password1': {'write_only': True},
+            'password2': {'write_only': True},
+        }
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2', None)
+        password = validated_data.pop('password1', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
