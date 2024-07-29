138d59b9794a79ab89b0edb71e3bde5eedb79bf3
Tamirat <amenguda@gmail.com>
2024-07-29 03:37:21 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Expenses from "./components/Expenses";
+import AddUsers from "./pages/ManageUsers";
+import CreatePost from "./pages/CreateProjects";
+import Footer from "./components/Footer";
+import UpdatePost from "./pages/UpdatePost";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import PrivateRoute from "./components/PrivateRoute";
+import Projects from "./pages/Projects";
+import Group from "./pages/Group";
+import Apply from "./pages/Apply";
+import EditIncomeForm from "./components/EditIncomeForm";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/group" element={<Group />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />  
+        <Route path="/editincome/:incomeId" element={<EditIncomeForm />} />  
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-expense" element={<Expenses />} />
+          <Route path="/apply/:projectId" element={<Apply />} />  
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-project" element={<CreatePost />} />
+          <Route path="/update-post/:projectId" element={<UpdatePost />} />
+          <Route path="/add-users" element={<AddUsers />} />
+          
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-
-export default App
